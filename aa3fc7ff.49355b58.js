92fea1bd2b2dd4fe2aef109238a25b44cd3b1f18
Sacha Ayoun <sachaayoun@gmail.com>
2020-02-25 14:04:01 +0000

    Deploy website version based on 83042dfbaa0d519319d4ce0c136642eaa5cffab5

---
+++ aa3fc7ff.49355b58.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{89:function(n,e,a){"use strict";a.r(e);var o=a(0),t=a.n(o),u=a(138),i={minimap:{enabled:!1}};e.default=function(n){var e=n.value,a=n.onChange,o=n.language;return t.a.createElement(u.ControlledEditor,{options:i,value:e,onChange:a,language:o})}}}]);
